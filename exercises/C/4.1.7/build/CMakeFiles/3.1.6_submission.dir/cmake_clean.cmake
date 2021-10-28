99f2c3fc79e50e78f74b54dff682323f30535c25
devon.e.budzitowski2 <devon.e.budzitowski2.mil@mail.mil>
2021-10-28 15:23:46 +0100

    fixed file structure

---
+++ exercises/C/4.1.7/build/CMakeFiles/3.1.6_submission.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3.1.6_submission.dir/src/args.c.o"
+  "lib3.1.6_submission.pdb"
+  "lib3.1.6_submission.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3.1.6_submission.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exercises/C/4.1.7/build/CMakeFiles/3.1.6_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3.1.6_test.dir/tests/args_test.c.o"
+  "lib3.1.6_test.pdb"
+  "lib3.1.6_test.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3.1.6_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
