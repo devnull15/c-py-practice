f2b9951c21d975c8bb61618241fd910fa75a3f58
aheath <3705-aheath@users.noreply.gitlab.devforce.disa.mil>
2022-01-13 10:08:32 -0500

    Create clean branch for 4.3.7

---
+++ exercises/C/4.3.7/tests/stacks_test.h
@@ -0,0 +1,56 @@
+/*
+    Reference the CUnit Users Guide:
+    http://cunit.sourceforge.net/doc/writing_tests.html#tests
+
+    Step 1. Create the test cases
+    Step 2. _add the test suite to the registry
+    Step 3. _add the test to the suite.
+*/
+
+/**
+ * @brief tests create_stack() as an individual function. 
+ * 
+ */
+void jqr_test_create_stack(void);
+
+/**
+ * @brief tests push() as an individual function. 
+ * 
+ */
+void jqr_test_push(void);
+
+/**
+ * @brief tests pop_at() as an individual function. 
+ * 
+ */
+void jqr_test_pop_at(void);
+
+/**
+ * @brief tests pop() as an individual function. 
+ * 
+ */
+void jqr_test_pop(void);
+
+/**
+ * @brief tests peek_at() as an individual function. 
+ * 
+ */
+void jqr_test_peek_at(void);
+
+/**
+ * @brief tests peek() as an individual function. 
+ * 
+ */
+void jqr_test_peek(void);
+
+/**
+ * @brief tests print_stack() as an individual function
+ * 
+ */
+void jqr_test_print_stack(void);
+
+/**
+ * @brief tests clear() as an individual function
+ * 
+ */
+void jqr_test_clear(void);
