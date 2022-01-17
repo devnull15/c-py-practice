2617b8e279d1e3ce30d4d38911e4ffd7c92e756f
devon.e.budzitowski2 <devon.e.budzitowski2.mil@mail.mil>
2022-01-17 22:51:58 +0000

    moved to clean branch

---
+++ exercises/Python/4.2.7/doc_strings/conditional.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+"""JQR Items Covered
+
+3.2.7
+title: demonstrate skill in creating and implementing conditional
+statements, expressions and contructs
+condition: C
+standard: C
+sub_items:
+    a - for loop
+    b - while loop
+    c - if statement
+    d - if->else statement
+    e - if->elif->else statement
+
+Brief: Add function parameters and your own implementation to complete
+       the functions. Test your solution against the tester.py file.
+"""
+
+
+def forLoop(l1):
+    """
+    Write a function that squares each value in a list.
+    Example:
+    Input: [2, 4, 6, 8]
+    Output: [4, 16, 36, 64]
+    """
+    pass
+
+
+def whileLoop(l2):
+    """
+    Write a function that counts down to 5 by taking the input
+    and appending the countdown to a list.  Return the list.
+    Example:
+    Input: 10
+    Output: [10, 9, 8, 7, 6, 5]
+    """
+    pass
+
+
+def ifStatement(l3):
+    """
+    Write a function that takes in a list of numbers and removes any values
+    that are divisible by 3
+    Example:
+    Input: [1, 2, 3, 4]
+    Output: [1, 2, 4]
+    """
+    pass
+
+
+def ifElseStatement(num):
+    """
+    A prime number is a natural number greater than 1 that is not a product
+    of two smaller natural numbers. Write a function that returns True if the
+    input is a prime number and False otherwise.
+    Example:
+    Input: 5
+    Result: True
+    """
+    pass
+
+
+def ifElifElseStatement(test_string):
+    """Write a function that accepts a string and returns the number of
+    digits and letters in a list, where the first entry is digits, and
+    the second entry is letters.
+    Example:
+    Input: abcd12
+    Result: [2, 4]
+    """
+    pass
