40022694aad27bbcae4691881cc9b78e01fe7f75
devon.e.budzitowski2 <devon.e.budzitowski2.mil@mail.mil>
2022-01-28 18:48:42 +0000

    create clean branch for 4.6.2

---
+++ exercises/C/4.6.2/build/CMakeFiles/3.6.2_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3.6.2_test.dir/tests/secure_test.c.o"
+  "lib3.6.2_test.pdb"
+  "lib3.6.2_test.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3.6.2_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
