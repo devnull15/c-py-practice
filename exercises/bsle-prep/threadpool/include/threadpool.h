b0f147c1bcd8b65592bf0cf6b342d3f09e6fdb6d
devon.e.budzitowski2 <devon.e.budzitowski2.mil@mail.mil>
2022-03-15 16:09:52 +0000

    barr c'd

--- exercises/bsle-prep/threadpool/include/threadpool.h
+++ exercises/bsle-prep/threadpool/include/threadpool.h
@@ -4,9 +4,66 @@
 #define _THREADPOOL_H
 
 #include <stdint.h>
+#include <pthread.h>
+#include <ll.h>
+#include <stdatomic.h>
 
+/**
+ * @brief struct for a job or task to be put into the job queue
+ *        and executed by the threads in the pool
+ *
+ * @param jobdef - function definition of the job
+ *
+ * @param args - pointer to arguments for the job function
+ *
+ */
+typedef struct job_
+{
+    void (*jobdef)(void *);
+    void *args;
+} job;
 
-typedef struct threadpool_ threadpool;
+/**
+ * @brief contains a linked list of jobs and a lock to ensure
+ *        thread safety
+ *
+ * @param lock - mutex to prevent race conditions on the queue
+ *
+ * @param queue - pointer to the head of the linked list / queue
+ *
+ * @param len - current number of job nodes in the queue
+ *
+ */
+typedef struct jobqueue_
+{
+    pthread_mutex_t lock;
+    ll *            queue;
+    atomic_uint     len;
+} jobqueue;
+
+/**
+ * @brief struct for the thread pool contains a number of worker
+ *        threads and a job queue; threads execute jobs on the queue
+ *        in a FIFO manner; the number of threads cannot be changed
+ *        after initialization
+ *
+ * @param threads - array of worker threads
+ *
+ * @param jq - pointer to the job queue
+ *
+ * @param keepalive - used to signal the threads to stop waiting for jobs
+ *
+ * @param nthreads - number of worker threads in the pool
+ *
+ */
+typedef struct threadpool_
+{
+    pthread_t * threads;
+    jobqueue *  jq;
+    atomic_bool keepalive;
+    atomic_uint nthreads;
+} threadpool;
+/* STRUCTS */
 
 /**
  * @brief initilizes the thread pool
@@ -16,8 +73,7 @@ typedef struct threadpool_ threadpool;
  * @return pointer to intilized thread pool or NULL on error
  *
  */
-threadpool* thpool_init(int num_threads);
-
+threadpool *thpool_init(int num_threads);
 
 /**
  * @brief add jobs to the thread pool
@@ -31,11 +87,10 @@ threadpool* thpool_init(int num_threads);
  * @return 0 on success; nonzero on error
  *
  */
-int thpool_add_job(threadpool* pool, void (*jobdef)(void *), void* args);
-
+int thpool_add_job(threadpool *pool, void (*jobdef)(void *), void *args);
 
 /**
- * @brief stops execution of the thread pool; 
+ * @brief stops execution of the thread pool;
  *        frees resources and join threads
  *
  * @param pool - thread pool to be stopped and freed
@@ -43,6 +98,6 @@ int thpool_add_job(threadpool* pool, void (*jobdef)(void *), void* args);
  * @return 0 on success; nonzero on error
  *
  */
-int thpool_destroy(threadpool* pool);
+int thpool_destroy(threadpool *pool);
 
 #endif /* _THREADPOOL_H */
